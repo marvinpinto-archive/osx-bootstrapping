d2dfdae6867a215114ac16d149ede451433995fb
Marvin Pinto <me@marvinp.ca>
2014-12-24 19:20:27 -0500

    Simplify the logic a bit and remove some of the redundant system calls

--- osx_apps/library/osx_defaults.py
+++ osx_apps/library/osx_defaults.py
@@ -62,17 +62,13 @@ def get_value(module, domain, key):
     rc, stdout, stderr = module.run_command(cmd, check_rc=True)
     return stdout.strip()
 
-def needs_update(module, domain, key, expected_value):
-    current_value = get_value(module, domain, key)
+def needs_update(current_value, expected_value):
     return not current_value == expected_value
 
 def update_value(module, domain, key, key_type, value):
-    if not needs_update(module, domain, key, value):
-        return False
     if not module.check_mode:
         cmd = '/usr/bin/defaults write %s "%s" -%s "%s"' % (domain, key, key_type, value)
         rc, stdout, stderr = module.run_command(cmd, check_rc=True)
-    return True
 
 def main():
 
@@ -92,14 +88,16 @@ def main():
     domain = module.params['domain']
     key = module.params['key']
     key_type = module.params['key_type']
-    value = module.params['value']
+    new_value = module.params['value']
 
     current_value = get_value(module, domain, key)
-    changed_marker = update_value(module, domain, key, key_type, value)
-    if changed_marker:
-        msg = '%s updated from %s to %s' % (key, current_value, value)
+    if needs_update(current_value, new_value):
+        update_value(module, domain, key, key_type, new_value)
+        msg = '%s updated from %s to %s' % (key, current_value, new_value)
+        changed_marker = True
     else:
-        msg = '%s unchanged - value %s' % (key, value)
+        msg = '%s unchanged - value %s' % (key, new_value)
+        changed_marker = False
 
     module.exit_json(msg=msg, changed=changed_marker)
 
